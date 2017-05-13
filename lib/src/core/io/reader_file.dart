f5a31f50ba138329f223ac9a86514198b6cca1bf
Christian Amani <alvesamani@gmail.com>
2017-05-13 18:45:05 +0000

    Implemented ReaderFile class

--- lib/src/core/io/reader_file.dart
+++ lib/src/core/io/reader_file.dart
@@ -1,32 +1,62 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:dartedb/src/core/io/action_file_system.dart';
+import 'package:logging/logging.dart';
 
 class ReaderFile extends ActionFileSystem
 {
-
+  Logger _LOG = new Logger("ReaderFile");
   @override
   File create(String url) {
-    // TODO: implement create
+    if(url != null)
+    {
+      File file = new File(url);
+      Future<File> future = file.create();
+      future.then((f) => _LOG.info("File created with success")
+        ,onError: () => _LOG.severe("Error during creation of file"));
+      return file;
+    }
+    return null;
   }
 
   @override
   List<File> createSeveral(List<String> listUrl) {
-    // TODO: implement createSeveral
+    if(listUrl.isNotEmpty)
+    {
+      List<File> list = new List<File>();
+      listUrl.forEach((s) => list.add(create(s)));
+      return list;
+    }
+    return null;
   }
 
   @override
   bool fileExist(String url) {
-    // TODO: implement fileExist
+    if(url != null)
+    {
+      File file = new File(url);
+      return file.existsSync();
+    }
+    return false;
   }
 
   @override
   void delete(String url) {
-    // TODO: implement delete
+    if(url != null)
+    {
+      File file = new File(url);
+      Future<File> future = file.delete();
+      future.then((f) => _LOG.info("File deleted with success")
+      ,onError: () => _LOG.severe("Error during deleted of file"));
+    }
   }
 
   @override
   void deleteSeveral(List<String> listUrl) {
-    // TODO: implement deleteSeveral
+    if(listUrl.isNotEmpty)
+    {
+      listUrl.forEach((s) => delete(s));
+    }
   }
 
 }
