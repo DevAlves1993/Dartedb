28e97c944312cc458d2e40516dd43170a8734c9c
Christian Amani <alvesamani@gmail.com>
2017-05-13 15:19:39 +0000

    implemented FolderSystem class

--- lib/src/core/io/folder_system.dart
+++ lib/src/core/io/folder_system.dart
@@ -1,21 +1,45 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:dartedb/src/core/io/action_folder.dart';
+import 'package:logging/logging.dart';
+
 
 class FolderSystem extends ActionFolder
 {
+  final Logger _LOG = new Logger("FolderSystem");
 
   @override
   Directory create(String url) {
-    // TODO: implement create
+    if(url != null)
+    {
+      Directory dir = new Directory(url);
+      Future<Directory> directory = dir.create();
+      directory.whenComplete(() => _LOG.info("Directory created with success"));
+      return dir;
+    }
+    return null;
   }
 
   @override
   void delete(String url) {
-    // TODO: implement delete
+    if(url != null)
+    {
+      Directory dir = new Directory(url);
+      if(folderExist(url))
+      {
+        Future<FileSystemEntity> fileSystem = dir.delete();
+        fileSystem.whenComplete(() => _LOG.info("Directory deleted with success"));
+      }
+    }
   }
 
   @override
   bool folderExist(String url) {
-    // TODO: implement folderExist
+    if(url != null)
+    {
+      Directory dir = new Directory(url);
+      return dir.existsSync();
+    }
+    return false;
   }
 }
